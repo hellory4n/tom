3f4469ffc6ca6e111550e5d4f074fd9044c156e0
hellory4n <95989937+hellory4n@users.noreply.github.com>
2022-05-20 09:22:28 -0500

    Create tom.py

---
+++ tom.py
@@ -0,0 +1,35 @@
+import random
+import discord
+from transformers import pipeline
+generator = pipeline('text-generation', model='EleutherAI/gpt-neo-125M')
+
+client = discord.Client(status=discord.Status.online, activity=discord.Game(name="Epok Bus Sim 2"))
+
+@client.event
+async def on_ready():
+    print("I'm ready :D")
+
+    # Sentences were suggested by these people:
+    # Tha Muffin
+    # hellory4n (me :D )
+    # Rubi
+    # RThreeFive
+    sentences = [
+        "Your mom's fat",
+        "Pizza",
+        "Walmart is an epok place",
+        "Mexico is Mexico",
+        "Windows 11 brings you closer to the thing you love.",
+        "I have commited several warcrimes in Winchestertonville, USA",
+        "I like to eat my chair",
+        "What is apple"
+    ]
+
+    prompt = random.choice(sentences)
+    res = generator(prompt, max_length=100, do_sample=True, temperature=0.9)
+    message = res[0]['generated_text']
+
+    send_message_on = client.get_channel() # Put a channel id here
+    await send_message_on.send(message)
+
+client.run("token")
